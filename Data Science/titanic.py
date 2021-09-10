4f2357e11bc39ca3ccd45447670c95e9e14583ab
Ayuba Tahiru <58880393+Tayuba@users.noreply.github.com>
2021-09-10 20:42:03 +0200

    websitee

---
+++ Data Science/titanic.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import sklearn
+import matplotlib.pyplot as plt
+
+"""Loading Data"""
+df = pd.read_csv("train.csv")
+print(df)
+
---
+++ FLASK WEB APP TIPS/website/static/index.js
@@ -0,0 +1,18 @@
+//function deleteNote(noteId){
+//    fetch("/delete-note",{
+//    method: "POST",
+//    body: JSON.stringify({ noteId: noteId }),
+//    }).then((_res) => {
+//    window.location,href = "/";
+//    });
+//}
+
+
+function deleteNote(noteId) {
+  fetch("/delete-note", {
+    method: "POST",
+    body: JSON.stringify({ noteId: noteId }),
+  }).then((_res) => {
+    window.location.href = "/";
+  });
+}
